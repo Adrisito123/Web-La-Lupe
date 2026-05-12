d40be8acb7550cd08c2a9e0aeeff2666b0dd12ce
Adrisito123 <131748443+Adrisito123@users.noreply.github.com>
2026-05-12 17:36:39 +0200

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<?php if (isset($_SESSION['mensaje'])): ?>
-    <div class="notificacion-universal <?php echo $_SESSION['tipo_mensaje']; ?>">
-        <div class="contenido-noti">
-            <span><?php echo $_SESSION['mensaje']; ?></span>
-        </div>
-        <button onclick="this.parentElement.remove()" class="btn-cerrar-noti">&times;</button>
-    </div>
-    <?php unset($_SESSION['mensaje'], $_SESSION['tipo_mensaje']); ?>
-<?php endif; ?>
